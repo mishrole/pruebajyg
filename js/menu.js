df6af0e5f35b686d4410abe0579b2fa4988cb404
Mitchell Rodríguez <mitrodle@gmail.com>
2017-03-02 16:22:04 -0500

    Primer commit

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var iconMenu = document.getElementById('icon-menu');
+var nav = document.getElementById('nav');
+
+iconMenu.addEventListener('click', function(){
+	nav.classList.toggle('mostrar');
+})
